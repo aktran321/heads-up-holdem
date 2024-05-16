9402870be59458597d552cff253796a17e71710e
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-05-16 10:55:02 -0400

    added styling to the cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,31 @@
-// src/components/Card.tsx
-import React from 'react'
-import { Card as CardType } from '../models/card'
+import React from 'react';
+import { Card as CardType } from '../models/card';
+import './Card.css'; 
 
 interface CardProps {
-  card: CardType
+  card: CardType;
 }
+const suitSymbols: { [key: string]: string } = {
+  'Hearts': '♥',
+  'Diamonds': '♦',
+  'Clubs': '♣',
+  'Spades': '♠'
+};
 
 const Card: React.FC<CardProps> = ({ card }) => {
+  const suitSymbol = suitSymbols[card.suit]
   return (
-    <div className='card'>
-      {card.rank} of {card.suit}
+    <div className={`card ${card.suit}`}>
+      <div className="card-top">
+        <span className="card-rank">{card.rank}</span>
+        <span className="card-suit">{suitSymbol}</span>
+      </div>
+      <div className="card-bottom">
+        <span className="card-rank">{card.rank}</span>
+        <span className="card-suit">{suitSymbol}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
